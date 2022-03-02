9a004111d1824bfbe3113ebcdb8e67171db81cbe
on9ue <95660939+on9ue@users.noreply.github.com>
2022-03-02 16:34:31 +0000

    許可ホストを追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "8d85c6f9b60748da8820ede70e38ad1f.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
